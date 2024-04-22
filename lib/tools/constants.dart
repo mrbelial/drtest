2e7b862e6b750f776358dd81d59025d2c5a98cda
Mahdi Shojaei <mahdi.shojaei68@gmail.com>
2024-04-22 18:17:32 +0330

    change app name

--- lib/tools/constants.dart
+++ lib/tools/constants.dart
@@ -13,7 +13,7 @@ part of 'core.dart';
 String get appName => _getAppName();
 
 String _getAppName() {
-  return "GymTrainer";
+  return "DR Test";
   // switch (_currentApp) {
   //   case AppConfig.ryanex:
   //     return "رایانکس";
